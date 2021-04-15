3bc41f8647c5cbff76ac83dbb900193aec31c3e5
real-bobby-tables <brandon.kristian.bell@gmail.com>
2021-04-15 16:14:27 -0500

    now using argparse for easy/safe argument parsing

--- CacheSimPart1.py
+++ CacheSimPart1.py
@@ -1,35 +1,77 @@
 import sys
 import math
+import argparse
+
+parser = argparse.ArgumentParser(description="Cache Simulator - CS 3853 Spring 2021 - Group 6")
+
+parser.add_argument('-s', '--CacheSize',
+    dest='CacheSize',
+    metavar='Cache Size',
+    type=int,
+    help="The size of the cache in kilobytes",
+    required=True) #from 1KB to 1MB
+
+parser.add_argument('-b', '--BlockSize',
+    dest='BlockSize',
+    metavar='Block Size',
+    type=int,
+    help="The size of the block in the cache",
+    required=True) # 4 bytes to 64 bytes
+
+parser.add_argument('-f', '--FileTrace',
+    dest='FileTrace',
+    metavar='Trace file',
+    type=str,
+    help="The name of the text file containing a trace",
+    required=True)
+
+parser.add_argument('-a', '--Associativity',
+    dest='Associativity',
+    metavar='Associativity',
+    type=int,
+    help="The associativity of the cache",
+    required=True) # 1,2,4,8,16
+
+parser.add_argument('-r', '--Replacement',
+    dest='Replacement',
+    metavar='Replacement policy',
+    type=str,
+    help="The replacement policy of this cache",
+    required=True) # RR, RND, or LRU
+
+
+args = parser.parse_args()
+
 
 repDict = {'RR':"Round Robin", 'RND':"Random", 'LRU':"Least Recently Used"}
 dataBus = 32
 
-#check inputs here
-#check argc
-if len(sys.argv) != 11:
-    print("ERROR: MISSING ARGUMENTS")
-    sys.exit(1)
+if args.Replacement not in repDict:
+    print("Bad replacement, please use either RR, RND, or LRU")
+    exit(1)
+
+
 
 #check file
-filetxt = open(sys.argv[2])
+filetxt = open(args.FileTrace)
 #check CS
 #check BS
 #check RP
 
 print("Cache Simulator - CS 3853 Spring 2021 - Group XX\n")
 
-print("Trace File: ", sys.argv[2],"\n")
+print("Trace File: ", args.FileTrace,"\n")
 
 print("***** Cache Input Parameters *****")
-print("Cache Size:\t\t\t\t\t",sys.argv[4],"KB")
-print("Block Size:\t\t\t\t\t",sys.argv[6],"bytes")
-print("Associativity Size:\t\t\t",sys.argv[8])
-print("Replacement Policy:\t\t\t",repDict[sys.argv[10]])
+print("Cache Size:\t\t\t\t\t",args.CacheSize,"KB")
+print("Block Size:\t\t\t\t\t",args.BlockSize,"bytes")
+print("Associativity Size:\t\t\t",args.Associativity)
+print("Replacement Policy:\t\t\t",repDict[args.Replacement])
 
-cSize = int(sys.argv[4])
+cSize = args.CacheSize
 cSizeBytes = pow(2, (math.log(cSize, 2)+10))
-bSize = int(sys.argv[6])
-aSoc = int(sys.argv[8])
+bSize = args.BlockSize
+aSoc = args.Associativity
 
 blocks = cSizeBytes / bSize
 indB = math.log(cSizeBytes / (bSize * aSoc), 2)
