5b0e8728f887c500e821e52059cb2c54456886b9
real-bobby-tables <brandon.kristian.bell@gmail.com>
2021-04-15 19:11:03 -0500

    parsing trace files

--- CacheSimPart1.py
+++ CacheSimPart1.py
@@ -53,7 +53,7 @@ if args.Replacement not in repDict:
 
 
 #check file
-filetxt = open(args.FileTrace)
+#filetxt = open(args.FileTrace)
 #check CS
 #check BS
 #check RP
@@ -72,27 +72,76 @@ cSize = args.CacheSize
 cSizeBytes = pow(2, (math.log(cSize, 2)+10))
 bSize = args.BlockSize
 aSoc = args.Associativity
+ZEROES = '00000000'
 
-blocks = cSizeBytes / bSize
-indB = math.log(cSizeBytes / (bSize * aSoc), 2)
-tagB = dataBus - indB - math.log(bSize, 2)
-rows = cSizeBytes / (bSize * aSoc)
-overhead = (blocks * 1 + blocks * tagB) / 8
-totalBytes = cSizeBytes + overhead
-impSize = (cSizeBytes + overhead) / 1024
-impSizeRU = math.ceil((cSizeBytes + overhead) / 1024)
-cost = round(impSizeRU * 0.09, 2)
+def calculate_cache_values():
 
-print("\n***** Cache Calculated Values *****\n")
+    blocks = cSizeBytes / bSize
+    indB = math.log(cSizeBytes / (bSize * aSoc), 2)
+    tagB = dataBus - indB - math.log(bSize, 2)
+    rows = cSizeBytes / (bSize * aSoc)
+    overhead = (blocks * 1 + blocks * tagB) / 8
+    totalBytes = cSizeBytes + overhead
+    impSize = (cSizeBytes + overhead) / 1024
+    impSizeRU = math.ceil((cSizeBytes + overhead) / 1024)
+    cost = round(impSizeRU * 0.09, 2)
 
-print("Total # Blocks:\t\t\t\t",blocks)
-print("Tag Size:\t\t\t\t\t",tagB,"bits")
-print("Index Size:\t\t\t\t\t",indB,"bits")
-print("Total # Rows:\t\t\t\t",rows)
-print("Overhead Size:\t\t\t\t",overhead,"bytes")
-print("Implementation Memory Size:\t",impSize,"KB","(",totalBytes,"bytes",")")
-print("Cost:\t\t\t\t\t\t $"+str(cost))
+    print("\n***** Cache Calculated Values *****\n")
 
+    print("Total # Blocks:\t\t\t\t",blocks)
+    print("Tag Size:\t\t\t\t\t",tagB,"bits")
+    print("Index Size:\t\t\t\t\t",indB,"bits")
+    print("Total # Rows:\t\t\t\t",rows)
+    print("Overhead Size:\t\t\t\t",overhead,"bytes")
+    print("Implementation Memory Size:\t",impSize,"KB","(",totalBytes,"bytes",")")
+    print("Cost:\t\t\t\t\t\t $"+str(cost))
+
+def parse_instruction_line(line):
+    instr_arr = line.split()
+    print(instr_arr)
+    return None
+
+def parse_data_line(line):
+    data_arr = line.split()
+    src_tup = (data_arr[0], data_arr[1], data_arr[2])
+    dst_tup = (data_arr[3], data_arr[4], data_arr[5])
+    if (src_tup[0] is ZEROES or dst_tup[0] is ZEROES):
+        print("Found zeroes, skipping")
+        return None
+    print(f'Got src with addr: {src_tup[1]}{src_tup[2]}, Dest with addr: {dst_tup[1]}{dst_tup[2]}')
+    return None
+
+def read_trace(tr):
+    trace = []
+    with open(tr, "r") as f:
+        for line in f:
+            trace.append(line.strip())
+    #print(trace)
+    return trace
+
+def step(tr, instr_idx, data_idx):
+    parse_instruction_line(tr[instr_idx])
+    parse_data_line(tr[data_idx])
+
+
+def simulate():
+    print("\n\n\n***** Beginning Simulation ******")
+    trace = read_trace(args.FileTrace)
+    step(trace, 0, 1)
+
+
+
+
+
+if __name__ == '__main__':
+    calculate_cache_values()
+    simulate()
+
+
+
+
+
+'''
 print("\n***** Trace File Contents *****\n")
 i = 0
 n = 0
@@ -105,3 +154,4 @@ for line in filetxt:
         print("0x"+leng+ " ("+addr+")")
         n += 1
     i += 1
+#'''
